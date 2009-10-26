bcc7f2ed8e18173cb3e3874dbfda5b173dd25989
Damon Timm <damontimm@gmail.com>
2009-10-25 20:28:19 -0400

    Initial commit ...

---
+++ hitcount/__init__.py
@@ -0,0 +1,12 @@
+VERSION = (0, 1, 0, 'alpha', 1)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    else:
+        if VERSION[3] != 'final':
+            version = '%s %s' % (version, VERSION[3])
+    return version
+
+__version__ = get_version()
