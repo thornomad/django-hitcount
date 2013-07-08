a82cd34b0b5cba2ff4918896e120bd64e718601e
Basil Shubin <basil.shubin@gmail.com>
2013-07-08 18:14:26 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import os
-from setuptools import setup, find_packages
- 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
- 
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 setup(
-    name = "django-hitcount",
-    version = "0.2",
-    url = 'http://github.com/thornomad/django-hitcount',
+    name = "django-hitcount-lite",
+    version = "0.2.1",
+    packages=['hitcount'],
     license = 'BSD',
     description = "Django hit counter application that tracks the number of hits/views for chosen objects",
-    long_description = read('README.md'),
- 
+    long_description = README,
     author = 'Damon Timm',
     author_email = 'damontimm@gmail.com',
- 
-    packages = find_packages(),
-    
+    maintainer='Basil Shubin',
+    maintainer_email='basil.shubin@gmail.com',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
@@ -26,4 +26,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    zip_safe=False,
 )
