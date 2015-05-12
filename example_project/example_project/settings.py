8adb1d202508d6d941ea9c199cd4f38bd0145874
Damon Timm <damontimm@gmail.com>
2015-05-12 09:54:14 -0700

    make example project timezone aware

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -78,8 +78,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-# for sqlite we have to disable timezones, sorry
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
