bdde1e84b623979085f1000221336f5ac8088054
Damon Timm <damontimm@gmail.com>
2015-05-26 10:58:47 -0700

    adding testing via runtests.py and py.test

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
+def pytest_configure():
+    from django.conf import settings
+
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3',
+                               'NAME': ':memory:'}},
+        SITE_ID=1,
+        SECRET_KEY='HitCounts Rock!',
+        USE_I18N=True,
+        USE_L10N=True,
+        MIDDLEWARE_CLASSES=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ),
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.staticfiles',
+
+            'hitcount',
+            'tests',
+        ),
+
+        # HitCount Variables
+        SESSION_SAVE_EVERY_REQUEST = True,
+        HITCOUNT_KEEP_HIT_ACTIVE = {'minutes': 60},
+        HITCOUNT_HITS_PER_IP_LIMIT = 0,
+        HITCOUNT_EXCLUDE_USER_GROUP = (),
+        HITCOUNT_KEEP_HIT_IN_DATABASE = {'days': 30},
+    )
+
+    try:
+        import django
+        django.setup()
+    except AttributeError:
+        pass
+
+    # so we can reuse this function when testing directly from Django
+    # via: ./runtests.py --django
+    return settings
