3d420553d756297842ac2191bb819430c4bc0b03
Damon Timm <damontimm@gmail.com>
2015-06-27 20:49:28 -0700

    updated example project for python3

--- example_project/blog/models.py
+++ example_project/blog/models.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
 
-    def __unicode__(self):  # Python 3: def __str__(self):
+    def __str__(self):
         return self.title
--- example_project/blog/views.py
+++ example_project/blog/views.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from blog.models import Post
+
 
 def index(request):
     post_list = Post.objects.all()[:5]
@@ -14,9 +18,9 @@ def index(request):
     return HttpResponse(template.render(context))
 
 
-@ensure_csrf_cookie #this is required for views that use django-hitcount
+@ensure_csrf_cookie  # this is required for views that use django-hitcount
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     post_list = Post.objects.all()[:5]
     return render(request, 'blog/post.html',
-        {'post' : post, 'post_list': post_list })
+                  {'post': post, 'post_list': post_list})
