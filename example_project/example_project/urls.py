8c2805af401319c17d60c55d19628f2b2991cc41
Damon Timm <damontimm@gmail.com>
2015-10-04 09:22:21 -0700

    replaces patterns function in example project

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
 
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 from blog import views
-from hitcount.views import update_hit_count_ajax
 
+admin.autodiscover()
 
-urlpatterns = patterns('',
-
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', views.index, name="index"),
@@ -16,5 +15,4 @@ urlpatterns = patterns('',
 
     # for our built-in ajax post view
     url(r'hitcount/', include('hitcount.urls', namespace='hitcount')),
-
-)
+]
