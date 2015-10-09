02865fb1df67169322329e1d57b601be1ffd858b
Damon Timm <damontimm@gmail.com>
2015-10-08 21:42:42 -0700

    reworked runtests.py to include flake8

--- runtests.py
+++ runtests.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import sys
 import os
+import subprocess
 
 import pytest
-
-DEFAULT_ARGS = ['tests']
+# from flake8.main import main as flake8_main
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -32,14 +33,38 @@ def run_django_tests(args):
     except ImproperlyConfigured:
         sys.exit('TEST #FAIL: The --django arg has only been tested with 1.8')
 
-if __name__ == "__main__":
-    args = DEFAULT_ARGS.append(sys.argv)
 
-    try:
-        sys.argv.remove('--django')
-    except ValueError:
-        args = DEFAULT_ARGS.append(sys.argv)
-        exit_on_failure(pytest.main(args))
-    else:
-        args = DEFAULT_ARGS.append(sys.argv)
-        run_django_tests(args)
+def flake8_main(args):
+    print('Flake8 code linting ...')
+    ret = subprocess.call(['flake8'] + args)
+    print('Flake8 failed' if ret else 'Flake8 passed')
+    return ret
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('extra_args', metavar="args", type=str, nargs='*',
+                        help='Additional arguments to pass to the test runner')
+    parser.add_argument('--django',
+                        help='Use Django test runner (>=1.8). Additional arguments in the form of tests.test_filename',
+                        action='store_true')
+    parser.add_argument('--no-flake8',
+                        help='Disable flake8 testing',
+                        action='store_true')
+    parser.add_argument('--flake8-only',
+                        help='Only perform flake8 testing',
+                        action='store_true')
+
+    args = parser.parse_args()
+    extra_args = args.extra_args
+
+    if not args.flake8_only:
+        if args.django:
+            run_django_tests(extra_args)
+            extra_args = []
+        else:
+            exit_on_failure(pytest.main(extra_args))
+
+    if not args.no_flake8:
+        exit_on_failure(flake8_main(extra_args))
