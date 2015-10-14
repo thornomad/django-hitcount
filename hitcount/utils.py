adf474efc40b9073057ea1c094df7c4602f5c7fe
Damon Timm <damontimm@gmail.com>
2015-10-14 14:19:50 -0700

    Use class-based views and a mixin

--- hitcount/utils.py
+++ hitcount/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import re
+import warnings
+
 
 # this is not intended to be an all-knowing IP address regex
 IP_RE = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
@@ -36,3 +38,10 @@ def get_ip(request):
             pass
 
     return ip_address
+
+
+class RemovedInHitCount13Warning(DeprecationWarning):
+    pass
+
+# enable warnings by default for our deprecated
+warnings.simplefilter("default", RemovedInHitCount13Warning)
--- hitcount/views.py
+++ hitcount/views.py
@@ -1,131 +1,153 @@
 # -*- coding: utf-8 -*-
 
-import json
+import warnings
 from collections import namedtuple
 
-from django.http import Http404, HttpResponse, HttpResponseBadRequest
+from django.http import Http404, JsonResponse, HttpResponseBadRequest
 from django.conf import settings
+from django.views.generic import View
 
 from hitcount.utils import get_ip
 from hitcount.models import Hit, HitCount, BlacklistIP, BlacklistUserAgent
+from hitcount.utils import RemovedInHitCount13Warning
+
+
+class HitCountMixin(object):
+    """
+    Mixin to evaluate a HttpRequest and a HitCount and determine whether or not
+    the HitCount should be incremented and the Hit recorded.
+    """
+
+    @classmethod
+    def hit_count(self, request, hitcount):
+        """
+        Called with a HttpRequest and HitCount object it will return a
+        namedtuple:
+
+        UpdateHitCountResponse(hit_counted=Boolean, hit_message='Message').
+
+        `hit_counted` will be True if the hit was counted and False if it was
+        not.  `'hit_message` will indicate by what means the Hit was either
+        counted or ignored.
+        """
+        UpdateHitCountResponse = namedtuple(
+            'UpdateHitCountResponse', 'hit_counted hit_message')
+
+        # as of Django 1.8.4 empty sessions are not being saved
+        # https://code.djangoproject.com/ticket/25489
+        if request.session.session_key is None:
+            request.session.save()
+
+        user = request.user
+        session_key = request.session.session_key
+        ip = get_ip(request)
+        user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
+        hits_per_ip_limit = getattr(settings, 'HITCOUNT_HITS_PER_IP_LIMIT', 0)
+        exclude_user_group = getattr(settings, 'HITCOUNT_EXCLUDE_USER_GROUP', None)
+
+        # first, check our request against the IP blacklist
+        if BlacklistIP.objects.filter(ip__exact=ip):
+            return UpdateHitCountResponse(
+                False, 'Not counted: user IP has been blacklisted')
+
+        # second, check our request against the user agent blacklist
+        if BlacklistUserAgent.objects.filter(user_agent__exact=user_agent):
+            return UpdateHitCountResponse(
+                False, 'Not counted: user agent has been blacklisted')
+
+        # third, see if we are excluding a specific user group or not
+        if exclude_user_group and user.is_authenticated():
+            if user.groups.filter(name__in=exclude_user_group):
+                return UpdateHitCountResponse(
+                    False, 'Not counted: user excluded by group')
+
+        # eliminated first three possible exclusions, now on to checking our database of
+        # active hits to see if we should count another one
+
+        # start with a fresh active query set (HITCOUNT_KEEP_HIT_ACTIVE)
+        qs = Hit.objects.filter_active()
+
+        # check limit on hits from a unique ip address (HITCOUNT_HITS_PER_IP_LIMIT)
+        if hits_per_ip_limit:
+            if qs.filter(ip__exact=ip).count() >= hits_per_ip_limit:
+                return UpdateHitCountResponse(
+                    False, 'Not counted: hits per IP address limit reached')
+
+        # create a generic Hit object with request data
+        hit = Hit(session=session_key, hitcount=hitcount, ip=get_ip(request),
+                  user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],)
+
+        # first, use a user's authentication to see if they made an earlier hit
+        if user.is_authenticated():
+            if not qs.filter(user=user, hitcount=hitcount):
+                hit.user = user  # associate this hit with a user
+                hit.save()
+
+                response = UpdateHitCountResponse(
+                    True, 'Hit counted: user authentication')
+            else:
+                response = UpdateHitCountResponse(
+                    False, 'Not counted: authenticated user has active hit')
+
+        # if not authenticated, see if we have a repeat session
+        else:
+            if not qs.filter(session=session_key, hitcount=hitcount):
+                hit.save()
+                response = UpdateHitCountResponse(
+                    True, 'Hit counted: session key')
+            else:
+                response = UpdateHitCountResponse(
+                    False, 'Not counted: session key has active hit')
+
+        return response
+
+
+class HitCountJSONView(View, HitCountMixin):
+    """
+    JSON response view to handle HitCount POST.
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            raise Http404()
+        return super(HitCountJSONView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        msg = "Hits counted via POST only."
+        return JsonResponse({'success': False, 'error_message': msg})
+
+    def post(self, request, *args, **kwargs):
+        hitcount_pk = request.POST.get('hitcountPK')
+
+        try:
+            hitcount = HitCount.objects.get(pk=hitcount_pk)
+        except:
+            return HttpResponseBadRequest("HitCount object_pk not working")
+
+        hit_count_response = self.hit_count(request, hitcount)
+        return JsonResponse(hit_count_response._asdict())
 
 
 def _update_hit_count(request, hitcount):
     """
-    Evaluates a request's Hit and corresponding HitCount object and,
-    after a bit of clever logic, either ignores the request or registers
-    a new Hit.
-
-    This is NOT a view!  But should be used within a view ...
-
-    Returns True if the request was considered a Hit; returns False if not.
+    Deprecated in 1.2. Use hitcount.views.Hit CountMixin.hit_count() instead.
     """
-
-    UpdateHitCountResponse = namedtuple(
-        'UpdateHitCountResponse', 'hit_counted hit_message')
-
-    # as of Django 1.8.4 empty sessions are not being saved
-    # https://code.djangoproject.com/ticket/25489
-    if request.session.session_key is None:
-        request.session.save()
-
-    user = request.user
-    session_key = request.session.session_key
-    ip = get_ip(request)
-    user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
-    hits_per_ip_limit = getattr(settings, 'HITCOUNT_HITS_PER_IP_LIMIT', 0)
-    exclude_user_group = getattr(settings, 'HITCOUNT_EXCLUDE_USER_GROUP', None)
-
-    # first, check our request against the IP blacklist
-    if BlacklistIP.objects.filter(ip__exact=ip):
-        response = UpdateHitCountResponse(
-            False, 'Not counted: user IP has been blacklisted')
-        return response
-
-    # second, check our request against the user agent blacklist
-    if BlacklistUserAgent.objects.filter(user_agent__exact=user_agent):
-        response = UpdateHitCountResponse(
-            False, 'Not counted: user agent has been blacklisted')
-        return response
-
-    # third, see if we are excluding a specific user group or not
-    if exclude_user_group and user.is_authenticated():
-        if user.groups.filter(name__in=exclude_user_group):
-            response = UpdateHitCountResponse(
-                False, 'Not counted: user excluded by group')
-            return response
-
-    # eliminated first three possible exclusions, now on to checking our database of
-    # active hits to see if we should count another one
-
-    # start with a fresh active query set (HITCOUNT_KEEP_HIT_ACTIVE)
-    qs = Hit.objects.filter_active()
-
-    # check limit on hits from a unique ip address (HITCOUNT_HITS_PER_IP_LIMIT)
-    if hits_per_ip_limit:
-        if qs.filter(ip__exact=ip).count() >= hits_per_ip_limit:
-            response = UpdateHitCountResponse(
-                False, 'Not counted: hits per IP address limit reached')
-            return response
-
-    # create a generic Hit object with request data
-    hit = Hit(session=session_key, hitcount=hitcount, ip=get_ip(request),
-              user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],)
-
-    # first, use a user's authentication to see if they made an earlier hit
-    if user.is_authenticated():
-        if not qs.filter(user=user, hitcount=hitcount):
-            hit.user = user  # associate this hit with a user
-            hit.save()
-
-            response = UpdateHitCountResponse(
-                True, 'Hit counted: user authentication')
-        else:
-            response = UpdateHitCountResponse(
-                False, 'Not counted: authenticated user has active hit')
-
-    # if not authenticated, see if we have a repeat session
-    else:
-        if not qs.filter(session=session_key, hitcount=hitcount):
-            hit.save()
-            response = UpdateHitCountResponse(
-                True, 'Hit counted: session key')
-        else:
-            response = UpdateHitCountResponse(
-                False, 'Not counted: session key has active hit')
-
-    return response
+    warnings.warn(
+        "hitcount.views._update_hit_count is deprecated. "
+        "Use hitcount.views.HitCountMixin.hit_count() instead.",
+        RemovedInHitCount13Warning
+    )
+    return HitCountMixin.hit_count(request, hitcount)
 
 
-def json_error_response(error_message):
-    response = json.dumps(dict(success=False, error_message=error_message))
-    return HttpResponse(response)
-
-
-def update_hit_count_ajax(request):
-    '''
-    Ajax call that can be used to update a hit count.
-
-    Ajax is not the only way to do this, but probably will cut down on
-    bots and spiders.
-
-    See template tags for how to implement.
-    '''
-
-    # make sure this is an ajax request
-    if not request.is_ajax():
-        raise Http404()
-
-    if request.method == "GET":
-        return json_error_response("Hits counted via POST only.")
-
-    hitcount_pk = request.POST.get('hitcountPK')
-
-    try:
-        hitcount = HitCount.objects.get(pk=hitcount_pk)
-    except:
-        return HttpResponseBadRequest("HitCount object_pk not working")
-
-    response = _update_hit_count(request, hitcount)
-
-    return HttpResponse(json.dumps(response), content_type="application/json")
+def update_hit_count_ajax(request, *args, **kwargs):
+    """
+    Deprecated in 1.2. Use hitcount.views.HitCountJSONView instead.
+    """
+    warnings.warn(
+        "hitcount.views.update_hit_count_ajax is deprecated. "
+        "Use hitcount.views.HitCountJSONView instead.",
+        RemovedInHitCount13Warning
+    )
+    view = HitCountJSONView.as_view()
+    return view(request, *args, **kwargs)
