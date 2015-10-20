0f60293408445fd48cc570e83641e1c5c2c27322
Damon Timm <damontimm@gmail.com>
2015-10-20 13:31:15 -0700

    added HitCountDetailView

--- hitcount/views.py
+++ hitcount/views.py
@@ -5,7 +5,8 @@ from collections import namedtuple
 
 from django.http import Http404, JsonResponse, HttpResponseBadRequest
 from django.conf import settings
-from django.views.generic import View
+from django.views.generic import View, DetailView
+from django.contrib.contenttypes.models import ContentType
 
 from hitcount.utils import get_ip
 from hitcount.models import Hit, HitCount, BlacklistIP, BlacklistUserAgent
@@ -126,6 +127,40 @@ class HitCountJSONView(View, HitCountMixin):
 
         hit_count_response = self.hit_count(request, hitcount)
         return JsonResponse(hit_count_response._asdict())
+
+
+class HitCountDetailView(DetailView, HitCountMixin):
+    """
+    HitCountDetailView provides an inherited DetailView that will inject the
+    template context with a `hitcount` variable giving you the number of
+    Hits for an object without using a template tag.
+
+    Optionally, by setting `count_hit = True` you can also do the business of
+    counting the Hit for this object (in lieu of using JavaScript).  It will
+    then further inject the response from the attempt to count the Hit into
+    the template context.
+    """
+    count_hit = False
+
+    def get_context_data(self, **kwargs):
+        context = super(HitCountDetailView, self).get_context_data(**kwargs)
+        if self.object:
+            ctype = ContentType.objects.get_for_model(self.object)
+            hit_count, created = HitCount.objects.get_or_create(
+                content_type=ctype, object_pk=self.object.pk)
+            hits = hit_count.hits
+            context['hitcount'] = {'pk': hit_count.pk}
+
+            if self.count_hit:
+                hit_count_response = self.hit_count(self.request, hit_count)
+                if hit_count_response.hit_counted:
+                    hits = hits + 1
+                context['hitcount']['hit_counted'] = hit_count_response.hit_counted
+                context['hitcount']['hit_message'] = hit_count_response.hit_message
+
+            context['hitcount']['total_hits'] = hits
+
+        return context
 
 
 def _update_hit_count(request, hitcount):
