ca17897ab805068125597280cf06bac3ef169267
Damon Timm <damontimm@gmail.com>
2016-05-31 14:39:37 -0700

    remove bytes flag from initial migration
    Address #48

--- hitcount/migrations/0001_initial.py
+++ hitcount/migrations/0001_initial.py
@@ -62,7 +62,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('hits', models.PositiveIntegerField(default=0)),
                 ('modified', models.DateTimeField(auto_now=True)),
-                ('object_pk', models.PositiveIntegerField(verbose_name=b'object ID')),
+                ('object_pk', models.PositiveIntegerField(verbose_name='object ID')),
                 ('content_type', models.ForeignKey(related_name='content_type_set_for_hitcount',
                                                    to='contenttypes.ContentType')),
             ],
