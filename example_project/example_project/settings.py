8e721a86ea9ba833f48f6a6644bd01a6b94d0753
Damon Timm <damontimm@gmail.com>
2018-03-11 08:16:41 -0700

    update example project requirements

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'hitcount'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
