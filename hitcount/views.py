dae236a2305b486c115a21912431910a33cff9a4
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-10-09 13:11:17 +0200

    improving the logic of the HitCountMixin

--- hitcount/views.py
+++ hitcount/views.py
@@ -150,16 +150,16 @@ class HitCountDetailView(DetailView, HitCountMixin):
         if self.object:
             hit_count = get_hitcount_model().objects.get_for_object(self.object)
             hits = hit_count.hits
-            context['hitcount'] = {'pk': hit_count.pk}
+            context_hitcount = context['hitcount'] = {'pk': hit_count.pk}
 
             if self.count_hit:
                 hit_count_response = self.hit_count(self.request, hit_count)
                 if hit_count_response.hit_counted:
                     hits = hits + 1
-                context['hitcount']['hit_counted'] = hit_count_response.hit_counted
-                context['hitcount']['hit_message'] = hit_count_response.hit_message
+                context_hitcount['hit_counted'] = hit_count_response.hit_counted
+                context_hitcount['hit_message'] = hit_count_response.hit_message
 
-            context['hitcount']['total_hits'] = hits
+            context_hitcount['total_hits'] = hits
 
         return context
 
